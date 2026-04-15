7f6a700c70125d1d5398534f0376491fdae7b7c2
André Delaney <dre.delaney@gmail.com>
2026-04-15 07:50:23 -0400

    Add service worker controller change listener to reload on update

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// When a new service worker takes control, reload once so users
+// immediately see the latest version without having to close and reopen.
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    window.location.reload();
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
